8a2bd82560e91a753dd9223c9ad63e5aeaddbeb0
wenyigao6 <wenyigao@usc.edu>
2018-02-16 15:57:55 -0800

    changed servers-ip in clients to vm's

--- ee250-lab04/part2/udpClient.py
+++ ee250-lab04/part2/udpClient.py
@@ -7,7 +7,7 @@ def Main():
     # initial port = 1023
     port = 50001
 
-    server_addr = '127.0.0.1'
+    server_addr = '10.0.2.15'
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     s.bind((host,port))
